9ffdded69ec5e048ee050db0ad1541d2b57473f1
Angelica <angelica.arias33@gmail.com>
2024-04-14 22:14:44 -0500

    ADD: Main page added

--- react-ui-components/src/components/Main-page/Cards.jsx
+++ react-ui-components/src/components/Main-page/Cards.jsx
@@ -1,7 +1,48 @@
 import styles from "./cards.module.css";
+import michi from "../../../assets/tic-tac.svg";
+import pokemon from "../../../assets/poke-api.svg";
+import wordle from "../../../assets/wordle.svg";
+import youtube from "../../../assets/youtube.svg";
 
-const Cards = () => {
-  return <div>Cards</div>;
+const cardsData = [
+  { id: 1, imageUrl: michi, name: "ReactDev Tic-Tac-Toe" },
+  { id: 2, imageUrl: pokemon, name: "Poke Collection" },
+  { id: 3, imageUrl: wordle, name: "React Wordle" },
+  { id: 4, imageUrl: youtube, name: "Video Feed" },
+];
+
+const Cards = ({ handleShow }) => {
+  return (
+    <div className={styles.card_container}>
+      {cardsData.map((card) => (
+        <div
+          className={styles.container}
+          key={card.id}
+          onClick={() => handleShow(card.name)}
+        >
+          <img src={card.imageUrl} alt={`Card ${card.id}`} />
+          <div className={styles.info_card}>
+            <h2>{card.name}</h2>
+            <div className={styles.content}>
+              <div className={styles.content_info}>
+                <p className={styles.feature}>useSate</p>
+                <p className={styles.feature}>useEffect</p>
+                <p className={styles.feature}>Custom Hooks</p>
+              </div>
+              <div className={styles.content_info}>
+                <p className={styles.feature}>LocalStorage</p>
+                <p className={styles.feature}>CSS Modules</p>
+              </div>
+              <div className={styles.content_info}>
+                <p className={styles.feature}>otherFeature</p>
+                <p className={styles.feature}>otherFeature</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Cards;
